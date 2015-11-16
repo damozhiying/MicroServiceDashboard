7c2d08c7d14b332994bcaa40160bb028ebb2311f
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-11-16 07:44:51 +0700

    Intial Spring Boot initialisation

---
+++ src/main/java/io/dashboardhub/dashboard/microservice/MicroServiceDashboardApplication.java
@@ -0,0 +1,12 @@
+package io.dashboardhub.dashboard.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceDashboardApplication.class, args);
+    }
+}
