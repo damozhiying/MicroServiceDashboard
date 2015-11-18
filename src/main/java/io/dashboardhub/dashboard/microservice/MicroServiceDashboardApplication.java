4c9616af6401a80c7e909ac14a2bb5369acac0d5
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-11-18 15:06:40 +0700

    RestTemplate added to Application file

--- src/main/java/io/dashboardhub/dashboard/microservice/MicroServiceDashboardApplication.java
+++ src/main/java/io/dashboardhub/dashboard/microservice/MicroServiceDashboardApplication.java
@@ -2,11 +2,19 @@ package io.dashboardhub.dashboard.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroServiceDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroServiceDashboardApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+        return new RestTemplate();
     }
 }
