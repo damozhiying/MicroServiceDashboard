4495f14b25a9901e41d95fec35d4439d0557bfcc
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-11-25 05:59:27 +0000

    [INCOMPLETE] Websockets functionality added

--- src/main/java/io/dashboardhub/dashboard/microservice/controller/HomeController.java
+++ src/main/java/io/dashboardhub/dashboard/microservice/controller/HomeController.java
@@ -2,6 +2,8 @@ package io.dashboardhub.dashboard.microservice.controller;
 
 import io.dashboardhub.dashboard.microservice.service.ActuatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,20 @@ public class HomeController {
     @Autowired
     private ActuatorService actuatorService;
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @RequestMapping(value = {"/"}, method = RequestMethod.GET)
     public String list(Model model) {
 
         model.addAttribute("services", this.actuatorService.getServices());
 
         return "list";
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void pushData() {
+        this.messagingTemplate.convertAndSend(
+                "/actuators/latest", this.actuatorService.getServices());
     }
 }
